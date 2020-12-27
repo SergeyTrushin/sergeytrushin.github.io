217e86377e261ee1d6e4eb4a6a2dcd89f210ee22
SergeyTrushin <sergeytrushin-17@yandex.ru>
2020-12-27 15:07:59 +0400

    add swiper

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./css/style.less";
 
 
+
+
 const ageOnPage = document.querySelector("#age")
 
 
@@ -10,13 +12,54 @@ const ageOnPage = document.querySelector("#age")
 let dob = new Date(1995, 12, 17);
 
 let now = new Date(); //Текущя дата
-let today = new Date(now.getFullYear(), now.getMonth(), now.getDate()); //Текущя дата без времени
-let dobnow = new Date(today.getFullYear(), dob.getMonth(), dob.getDate()); //ДР в текущем году
+let today = new Date(now.getFullYear(), now.getMonth(), now.getDate()); 
+let dobnow = new Date(today.getFullYear(), dob.getMonth(), dob.getDate()); 
 
 let age = today.getFullYear() - dob.getFullYear();
-//Если ДР в этом году ещё предстоит, то вычитаем из age один год
 if (today < dobnow) {
-  age = age-1;
+	age = age-1;
 }
 
-ageOnPage.innerHTML = age
+ageOnPage.innerHTML = age;
+
+const swiper = new Swiper('.swiper-container', {
+  wrapperClass: 'swiper-wrapper',
+  slideClass: 'swiper-slide',
+  loop: false,
+  speed: 600,
+  observer: true,
+  observeParents: true,
+  parallax:true,
+  watchOverflow: true,
+  navigation: {
+    nextEl: '.swiper-next',
+    prevEl: '.swiper-prev',
+  },
+  pagination: {
+    el: '.slider-dots',
+    type: 'bullets',
+    clickable: true
+  },
+  breakpoints: {
+  	1150: {
+  		slidesPerView: 4,
+  		spaceBetween: 25,
+      slidesPerGroup: 1,
+  	},
+  	1024: {
+  		slidesPerView: 2,
+  		spaceBetween: 50,
+      slidesPerGroup: 1,
+  	},
+  	768: {
+  		slidesPerView: 2,
+  		spaceBetween: 40,
+      slidesPerGroup: 1,
+  	},
+    0: {
+  		slidesPerView: 2,
+  		spaceBetween: 5,
+      slidesPerGroup: 1,
+  	},
+  },
+})
--- webpack.config.js
+++ webpack.config.js
@@ -11,116 +11,116 @@ const isDev = process.env.NODE_ENV === 'development'
 const isProd = !isDev
 
 const optimization = ()=>{
-  const config = {
+	const config = {
 
-  }
+	}
 
-  if(isProd){
-    config.minimizer = [
-      new OptimizeCssAssetPlugin(),
-      new TerserWebpackPlugin()
-    ]
-  }
-  return config
+	if(isProd){
+		config.minimizer = [
+		new OptimizeCssAssetPlugin(),
+		new TerserWebpackPlugin()
+		]
+	}
+	return config
 } 
 
 module.exports = {
-  entry : "./src",
-  output : {
-    path : path.resolve(__dirname,'dist'),
-    filename : "bundle.js"
-  },
-  optimization : optimization(),
-  devServer: {
-      contentBase: path.join(__dirname, 'dist'),
-      port: 8080,
-    },
-    plugins: [
-      new MiniCssExtractPlugin({
-        filename : "style.css",
-        options : {
-           hmr : isDev,
-           reloadAll: true
-        }
-      }),
-      new HtmlWebpackPlugin({
-        template : "./src/index.html",
-        minify: {
-          collapseWhitespace: isProd
-        }
-      }),
-      new CleanWebpackPlugin(),
-      new MiniCssExtractPlugin({
-        filename : "style.css"
-      }),
-      new CopyWebpackPlugin({
-          patterns : [
-            {
-              from : path.resolve(__dirname, 'src/fonts'),
-              to : path.resolve(__dirname, 'dist/fonts')
-            },
-            {
-              from : path.resolve(__dirname, 'src/img'),
-              to : path.resolve(__dirname, 'dist/img')
-            }
-          ]
-        }),
+	entry : "./src",
+	output : {
+		path : path.resolve(__dirname,'dist'),
+		filename : "bundle.js"
+	},
+	optimization : optimization(),
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		port: 8080,
+	},
+	plugins: [
+	new MiniCssExtractPlugin({
+		filename : "style.css",
+		options : {
+			hmr : isDev,
+			reloadAll: true
+		}
+	}),
+	new HtmlWebpackPlugin({
+		template : "./src/index.html",
+		minify: {
+			collapseWhitespace: isProd
+		}
+	}),
+	new CleanWebpackPlugin(),
+	new MiniCssExtractPlugin({
+		filename : "style.css"
+	}),
+	new CopyWebpackPlugin({
+		patterns : [
+		{
+			from : path.resolve(__dirname, 'src/fonts'),
+			to : path.resolve(__dirname, 'dist/fonts')
+		},
+		{
+			from : path.resolve(__dirname, 'src/img'),
+			to : path.resolve(__dirname, 'dist/img')
+		},
+		]
+	}),
 
-    ],
-    module: {
-      rules: [
-          {
-            test : /\.css$/,
-            use : [MiniCssExtractPlugin.loader, "css-loader"]
-          },
-          {
-            test : /\.less$/,
-            use : [
-                    {
-                      loader: MiniCssExtractPlugin.loader
-                    },
+	],
+	module: {
+		rules: [
+		{
+			test : /\.css$/,
+			use : [MiniCssExtractPlugin.loader, "css-loader"]
+		},
+		{
+			test : /\.less$/,
+			use : [
+			{
+				loader: MiniCssExtractPlugin.loader
+			},
 
-                    {
-                      loader: 'css-loader'
-                    },
-                    {
-                      loader: 'postcss-loader',
-                      options: {
-                          plugins: [
-                              autoprefixer()
-                          ],
-                          sourceMap: true
-                      }
-                    },
-                    {
-                      loader: 'less-loader'
-                    },
-                  ]
-          },
-          {
-            test: /\.(png|jpe?g|gif|svg)$/i,
-            use: [
-              {
-                loader: 'file-loader',
-                options:{
-                  outputPath : "img/",
-                  name: '[path][name].[ext]',
-                }
-              },
-            ],
-         },
-         {
-          test: /\.(woff(2)?|ttf|eot)/,
-          use: [
-              {
-                loader: 'file-loader',
-                options: {
-                  name: '[name].[ext]',
-                  outputPath: 'fonts/'
-                  }
-              }
-            ]
-          },
-      ]
-    },
+			{
+				loader: 'css-loader'
+			},
+			{
+				loader: 'postcss-loader',
+				options: {
+					plugins: [
+					autoprefixer()
+					],
+					sourceMap: true
+				}
+			},
+			{
+				loader: 'less-loader'
+			},
+			]
+		},
+		{
+			test: /\.(png|jpe?g|gif|svg)$/i,
+			use: [
+			{
+				loader: 'file-loader',
+				options:{
+					outputPath : "img/",
+					name: '[path][name].[ext]',
+				}
+			},
+			],
+		},
+		{
+			test: /\.(woff(2)?|ttf|eot)/,
+			use: [
+			{
+				loader: 'file-loader',
+				options: {
+					name: '[name].[ext]',
+					outputPath: 'fonts/'
+				}
+			}
+			]
+		},
+		]
+	},
 }
